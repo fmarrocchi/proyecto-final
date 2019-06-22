a73f98602122fd131816c723c0839b1266be9470
fmarrocchi <flor.marro@gmail.com>
2019-06-22 17:11:32 -0300

    armando formulario

--- front-end/src/app/busqueda/busqueda.component.ts
+++ front-end/src/app/busqueda/busqueda.component.ts
@@ -7,11 +7,35 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent implements OnInit {
-  public hashtags="";
+  public hashtag="";
+  public listaHashtags: string[] = [];
  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addHashtag(){
+    //La lista habria q vaciarla luego de llamar a la funcion
+    this.listaHashtags.push(this.hashtag)
+    this.hashtag=""
+    alert('elemento agregado a la lista: '+ this.listaHashtags)
+  }
+
+  getListaHashtags(){
+    return this.listaHashtags;
+  }
+
+  onSubmit() {
+    alert('Hashtag agregado !')
+  }
+
+  setHashtags(hashtag){
+    this.hashtags = hashtag;
+  }
+
+  getHashtags(){
+    return this.hashtags;
   }
 
 }
