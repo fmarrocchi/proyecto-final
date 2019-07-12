55f685bf8bd9c89c4313169793d2f90948926409
CarolinaRapetti <scrapetti@outlook.com>
2019-07-12 09:48:24 -0300

    Cambios en streaming, no funciona

--- back-end/api.py
+++ back-end/api.py
@@ -2,7 +2,8 @@ import os
 from datetime import date
 from flask import Flask, request, jsonify, abort
 from flask_restful import Api, Resource
-from flask_socketio import SocketIO, emit, Namespace
+from flask_socketio import SocketIO, emit, Namespace, join_room
+from threading import Thread
 import search_engine
 import twitter_tools
 import NLTools
@@ -16,7 +17,8 @@ LIMIT = 'limit'
 
 app = Flask(__name__)
 api = Api(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app, engineio_logger=True, async_mode='eventlet')
+thread = None
 
 class Home(Resource):
     def get(self):
@@ -64,63 +66,34 @@ class Search(Resource):
         print(resp.headers)
         return  resp
 
-class TwitterListener(StreamListener):
-    def __init__(self, statsObj, max_num_tweets):
-        self.counter = 0
-        self.max_num_tweets = max_num_tweets
-        self.tweets_list = []
-        self.statsObj = statsObj
-
-    def on_status(self, status):
-        print("status----------------")
-        print(status.text)
-        
-    def on_data(self, data):
-        try:        
-            all_data = json.loads(data) #to acces like a JSON object
-            tweet_received(all_data["text"])
-            self.counter += 1 #limitate number of tweets for program test  
-            
-            #self.tweets_list.append(all_data["text"])
-            print(self.counter)
-            print(all_data["text"])
-            #self.statsObj.add_tweet(all_data["text"])
-            #print(all_data) #para control de que es lo que encuentra
-            
-            if self.counter == self.max_num_tweets:
-                return False
-            return True
-
-        except BaseException as e:
-            print("Error on_data %s" % str(e))
-        return True
-
-    def on_error(self, status):
-        if status == 420:
-            # Returning False on_data method in case rate limit occurs.
-            return False
-        print(status)
-
 class Streamer(Namespace):
     def on_stream(self, data):
         print("-----data en streamer------")
         print(data)
-        twt = twitter_tools.TwitterTools() #instancia clase twitter_tools        
-        twt.stream_tweets('peru', 1)    #obtengo tweets en streaming
+        print(request.sid)
+        join_room(request.sid)
+        global thread
+        if thread is None:
+            thread = Thread(target=start_stream, args=("hola",5,request.sid))
+            thread.start()
         print("Mando mensaje")
-        emit('streamresponse', {'stream_tweet': "hola"}, namespace='/emotions-analyzer/streaming')
+        #emit('streamresponse', {'stream_tweet': "hola"}, namespace='/emotions-analyzer/streaming')
+        tweet_received("no se",request.sid)
         
+def start_stream(keys, cant, sid):
+    print("Empieza thread")
+    print(keys)
+    print(cant)
+    twt = twitter_tools.TwitterTools() #instancia clase twitter_tools        
+    twt.stream_tweets('peru', 1, sid)    #obtengo tweets en streaming
 
-
-    def on_disconnect(self):
-        pass
-
-def tweet_received(text):
+def tweet_received(text, sid):
         print("envio tweet")
-        socketio.emit('streamresponse', {'stream_tweet': text}, namespace='/emotions-analyzer/streaming')
+        socketio.emit('streamresponse', {'stream_tweet': text}, namespace='/emotions-analyzer/streaming', room=sid)
 
 api.add_resource(Search,'/emotions-analyzer')
 api.add_resource(Home,'/')
 socketio.on_namespace(Streamer('/emotions-analyzer/streaming'))
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
--- back-end/twitter_tools.py
+++ back-end/twitter_tools.py
@@ -32,10 +32,46 @@ class TwitterTools():
 
         return self.tweets
 
-    def stream_tweets(self, hash_tag_list, statsObj):
-        # This handles Twitter authentification and the connection to Twitter Streaming API
-        listener = api.TwitterListener(statsObj, max_num_tweets=20)
-        print("Inicia streaming")
+    def stream_tweets(self, hash_tag_list, max_tweets, sid):
+        # This handles Twitter authentification and the connection to Twitter Streaming API , is_async=True
+        listener = TwitterListener(8, sid)
         auth = self.twitter_autenticator.authenticate_twitter_app() 
         stream = Stream(auth, listener)
-        stream.filter(track=hash_tag_list,  languages=["es"], is_async=True)
+        stream.filter(track=hash_tag_list,  languages=["es"])
+
+class TwitterListener(StreamListener):
+    def __init__(self, max_num_tweets, sid):
+        self.counter = 0
+        self.max_num_tweets = max_num_tweets
+        self.tweets_list = []
+        self.sid = sid
+
+    def on_status(self, status):
+        print("status----------------")
+        print(status.text)
+        
+    def on_data(self, data):
+        try:        
+            all_data = json.loads(data) #to acces like a JSON object
+            api.tweet_received(all_data["text"], self.sid)
+            self.counter += 1 #limitate number of tweets for program test  
+            
+            #self.tweets_list.append(all_data["text"])
+            print(self.counter)
+            print(all_data["text"])
+            #self.statsObj.add_tweet(all_data["text"])
+            #print(all_data) #para control de que es lo que encuentra
+            
+            if self.counter == self.max_num_tweets:
+                return False
+            return True
+
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        if status == 420:
+            # Returning False on_data method in case rate limit occurs.
+            return False
+        print(status)
