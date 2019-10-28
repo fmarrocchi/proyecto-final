669ec9c3b9018064aaad80ea78ab42caededa2c1
CarolinaRapetti <scrapetti@outlook.com>
2019-10-28 10:35:18 -0300

    Lexicon actualizado

--- back-end/extras/parser.py
+++ back-end/extras/parser.py
@@ -4,19 +4,20 @@ import json
 """
 j = {}
 
-with open('C:/Users/scrap/Documents/Proyecto final/ProyGH/proyecto-final/back-end/extras/nrc_emotion_lexicon.csv') as fin:
+with open('./nrc_emotion_lexicon1.csv') as fin:
     reader = csv.DictReader(fin)
     for record in reader:
         j[record['Spanish (es)']] = {k:v for k,v in record.items() if k != 'Spanish (es)'}
             
-with open('C:/Users/scrap/Documents/Proyecto final/ProyGH/proyecto-final/back-end/extras/output.json','w') as fout:
+with open('./output.json','w') as fout:
     json.dump(j,fout,ensure_ascii=False)"""
 
-with xlrd.open_workbook('C:/Users/scrap/Documents/Proyecto final/ProyGH/proyecto-final/back-end/extras/nrc_emotion_lexicon.xlsx') as wb:
+"""with xlrd.open_workbook('./nrc_emotion_lexicon.xlsx') as wb:
     sh = wb.sheet_by_index(0)  # or wb.sheet_by_name('name_of_the_sheet_here')
-    with open('C:/Users/scrap/Documents/Proyecto final/ProyGH/proyecto-final/back-end/extras/nrc_emotion_lexicon.csv', 'w', newline='') as f:   # open('a_file.csv', 'w', newline="") for python 3
+    with open('./nrc_emotion_lexicon1.csv', 'w', newline='',encoding='utf-8') as f:   # open('a_file.csv', 'w', newline="") for python 3
         c = csv.writer(f)
+        c.writerow(["Spanish (es)","Positive","Negative","Anger","Anticipation","Disgust","Fear","Joy","Sadness","Surprise","Trust"])
         for r in range(1,sh.nrows):
-            c.writerow(sh.row_values(r))
+            c.writerow(sh.row_values(r,start_colx=1, end_colx=12))"""
 
 
